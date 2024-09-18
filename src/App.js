10a738c1d8dc6f3e1dff9d69f655c3ae68a9ce74
Lucas Rodrigues Grecco <lucas.rgrecco@gmail.com>
2024-09-18 08:19:34 -0300

    change function draw() to canvas

--- src/App.js
+++ src/App.js
@@ -28,17 +28,9 @@ function App() {
   console.log('PosX:', posX);
   console.log('PosY:', posY);
   console.log('Local:', posLocal);
-  const draw = (ctx, frameCount) => {
-    // ctx -> contexto 2D do canvas
-    // frameCount -> contador de frames - para animações
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = 'red';
-    ctx.beginPath();
-    ctx.arc(posX, posY, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-    ctx.fill();
-  }
+  
     if(posLocal === 'Bumbum'){
-      return <Canvas draw={draw} width="600" height="600" img='https://cdn.pixabay.com/photo/2020/05/04/10/21/background-5128585_1280.jpg'/>
+      return <Canvas posX={300} posY={300} width="600" height="600" img='https://cdn.pixabay.com/photo/2020/05/04/10/21/background-5128585_1280.jpg'/>
     }else{
       return (
       <>
@@ -49,13 +41,13 @@ function App() {
           pagination={{ clickable: true }}
         >
           <SwiperSlide>
-            <Canvas draw={draw} width="600" height="600" img='https://cdn.pixabay.com/photo/2020/05/04/10/21/background-5128585_1280.jpg' id={'canvas1'} className='canvas1'/>
+            <Canvas posX={300} posY={300} width="600" height="600" img='https://cdn.pixabay.com/photo/2020/05/04/10/21/background-5128585_1280.jpg' id={'canvas1'} className='canvas1'/>
           </SwiperSlide>
           <SwiperSlide>
-            <Canvas draw={draw} width="600" height="600" img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsilKSi86Q6pJIrgN29TCcC-ZiRG-_c6_B3Q&s' id={'canvas2'} className='canvas2'/>
+            <Canvas posX={150} posY={150} width="600" height="600" img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsilKSi86Q6pJIrgN29TCcC-ZiRG-_c6_B3Q&s' id={'canvas2'} className='canvas2'/>
           </SwiperSlide>
           <SwiperSlide>
-            <Canvas draw={draw} width="600" height="600" img='https://img.freepik.com/vetores-gratis/fundo-digital-do-espaco-azul-da-copia_23-2148821698.jpg' id={'canvas3'} className='canvas3'/>
+            <Canvas posX={300} posY={200} width="600" height="600" img='https://img.freepik.com/vetores-gratis/fundo-digital-do-espaco-azul-da-copia_23-2148821698.jpg' id={'canvas3'} className='canvas3'/>
           </SwiperSlide>
         </Swiper>
       </>
--- src/Canvas/Canvas.jsx
+++ src/Canvas/Canvas.jsx
@@ -3,7 +3,18 @@ import React from "react";
 import './styles.css';
 import { useCanvas } from "./CanvasHooks";
 
-export function Canvas({draw, img, id,...rest}){
+export function Canvas({posX, posY, img, id,...rest}){
+
+    const draw = (ctx, frameCount) => {
+        // ctx -> contexto 2D do canvas
+        // frameCount -> contador de frames - para animações
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(posX, posY, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
+        ctx.fill();
+      }
+
     const canvasRef = useCanvas(draw);
     console.log('img:', img);
     return (
